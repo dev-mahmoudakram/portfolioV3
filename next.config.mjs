eeda24002ecbb880b830d47cec6c9657d5806c2a
Vkrvm <mahakram35@gmail.com>
2026-04-26 00:55:16 +0300

    add standalone/server.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "standalone",
   serverExternalPackages: ["@prisma/client", "prisma"],
   sassOptions: {
     silenceDeprecations: ["legacy-js-api"]
